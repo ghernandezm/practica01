f96e1c4c1110cf1cca0a5a2023834ae6dc1dd4d1
tavohm <tavohm@valhalla>
2019-08-27 17:17:39 -0500

    Comenzamos la práctica

--- practica01/src/practica01/Practica01.java
+++ practica01/src/practica01/Practica01.java
@@ -1,10 +1,9 @@
 package practica01;
 
-
 public class Practica01 {
 
     public static void main(String[] args) {
-        // Escribe aquí el código de tu aplicación
+        System.out.println("Hola mundo en JAVA Git!");
     }
 
 }
